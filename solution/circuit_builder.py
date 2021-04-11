372b4805bbce7faf8ef313d66df9c48f79979477
J. Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2021-04-11 02:38:19 +0100

    Added Joanne code!

--- solution/circuit_builder.py
+++ solution/circuit_builder.py
@@ -1,4 +1,5 @@
 import cirq
+import numpy as np
 
 def convert_gatelist_to_cirq_ops(gatelist, target_qubits):
     # Implement once we know the exact input.
@@ -14,10 +15,10 @@ def convert_gatelist_to_cirq_ops(gatelist, target_qubits):
             # Check if SWAP is necessary
             # Check if q1 and q2 are neighbours
             if not q1.is_adjacent(q2):
-                #swap_operations, new_q2 = get_swap_qbits_list(q1, q2)
-                #ops.append(swap_operations)
+                swap_operations, new_q2 = get_swap_qbits_list(q1, q2)
+                ops.extend(swap_operations)
                 ops.append(gate(q1, new_q2))
-                #ops.append(swap_operations[::-1])
+                ops.extend(swap_operations[::-1])
             else:
                 ops.append(gate(q1, q2))
         else:
@@ -34,8 +35,8 @@ def get_swap_qbits_list(q1:cirq.GridQubit, q2:cirq.GridQubit):
     :param q2: cirq.GridQubit
     :return:
     """
-    q1_coord = [q1._col, q1._row]
-    q2_coord = [q2._col, q2._row]
+    q1_coord = [q1._row, q1._col]
+    q2_coord = [q2._row, q2._col]
 
     # Get the indices for each swap.
     # Returns [[[5,0],[5,1]], [[5,1],[5,2]] ... ]
@@ -43,6 +44,7 @@ def get_swap_qbits_list(q1:cirq.GridQubit, q2:cirq.GridQubit):
 
     # Create operations list based on the steps/jumps needed
     operations_list = []
+
     for step in short_path_steps:
         # step -> [[5,0], [5,1]]
         qi, qj = cirq.GridQubit(*step[0]), cirq.GridQubit(*step[1])
@@ -50,20 +52,59 @@ def get_swap_qbits_list(q1:cirq.GridQubit, q2:cirq.GridQubit):
         operations_list.append(swap)
 
     # Get the new q2 position (the last value of the last swap)
-    new_q2_coord = short_path_steps[-1][1]
+    new_q2_coord = short_path_steps[-1][-1]
     new_q2_position = cirq.GridQubit(*new_q2_coord)
     return operations_list, new_q2_position
 
-def get_short_path(coord1, coord2):
+
+def get_short_path(coord2, coord1):
     """
-    coords q1 = [5,0],    q2 = [5,4]
+    coords q1 = [4,3],    q2 = [4,1]
     Calculates the shortest path within a device structure.
     Returns an array with the series of swaps required.
     :param coord1:
     :param coord2:
     :return:
     """
-    return
+    final = []
+    path = [coord1]
+    diff = np.subtract(coord1, coord2)
+
+    if abs(diff[0]) != 0:
+        for i in range(abs(diff[1])):
+            if diff[0] < 0:
+                step = np.add(path[-1], [1, 0]).tolist()
+                path.append(step)
+
+            elif diff[0] > 0:
+                step = np.subtract(path[-1], [1, 0]).tolist()
+                path.append(step)
+
+            else:
+                pass
+    else:
+        pass
+
+    if abs(diff[1]) != 0:
+        for i in range(abs(diff[1])):
+            if diff[1] < 0:
+                step = np.add(path[-1], [0, 1]).tolist()
+                path.append(step)
+
+            elif diff[1] > 0:
+                step = np.subtract(path[-1], [0, 1]).tolist()
+                path.append(step)
+
+            else:
+                pass
+    else:
+        pass
+
+    for i in range(len(path) - 2):
+        final.append([path[i], path[i + 1]])
+
+    return final
+
 
 def convert_to_compatible_gates(operations, device):
     """
@@ -91,9 +132,9 @@ def convert_to_compatible_gates(operations, device):
     return operations
 
 
-def factorized_gate_list_to_qcircuit(
+def factorized_gate_list_to_listOps(
         factorized_gate_list,
-        # Of the form [[gate_2q, [qx, qy]], [gate_1q, [qx]], ... ]
+        # Of the form [[cirq.H, [qx, qy]], [gate_1q, [qx]], ... ]
         target_qubits,
         device = cirq.google.Sycamore,
 ):
@@ -105,7 +146,7 @@ def factorized_gate_list_to_qcircuit(
     """
 
     # Create a circuit on the device
-    circuit = cirq.Circuit()#device=device)
+    circuit = cirq.Circuit(device=device)
 
     # Convert list of gates to cirq operations
     ops = convert_gatelist_to_cirq_ops(factorized_gate_list, target_qubits)
@@ -127,6 +168,6 @@ factorized_gate_list = [[cirq.CZ,[0,1]], [cirq.CZ, [2,0]],
                         [cirq.CZ, [3,1]], [cirq.CZ, [2,3]], [cirq.H, [3]]]
 
 target_qubits = [cirq.GridQubit(4,i) for i in range(1,9)]
-ops = factorized_gate_list_to_qcircuit(factorized_gate_list, target_qubits)
+ops = factorized_gate_list_to_listOps(factorized_gate_list, target_qubits)
 print(ops)
 len(ops)
